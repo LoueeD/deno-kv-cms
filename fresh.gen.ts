45424e7d6241ff1c4b803b36a755bad05e7df5f5
lou <loueed@gmail.com>
2023-04-29 10:24:41 +0100

    simple cms base, test endpoint

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,16 +5,18 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/index.tsx";
-import * as $3 from "./routes/login.tsx";
+import * as $2 from "./routes/api/random-uuid.ts";
+import * as $3 from "./routes/index.tsx";
+import * as $4 from "./routes/login.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
     "./routes/api/joke.ts": $1,
-    "./routes/index.tsx": $2,
-    "./routes/login.tsx": $3,
+    "./routes/api/random-uuid.ts": $2,
+    "./routes/index.tsx": $3,
+    "./routes/login.tsx": $4,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
--- routes/index.tsx
+++ routes/index.tsx
@@ -5,19 +5,16 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <title>Deno KV CMS</title>
       </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
-        </p>
-        <Counter start={3} />
+      <div class="min-h-screen flex">
+        <div class="p-4 flex-shrink-0 border-r-1 border-gray-200 min-w-[280px]">
+          <h1 class="font-bold">Pages</h1>
+        </div>
+        <div class="p-4 flex-1 border-r-1 border-gray-200"></div>
+        <div class="p-4 flex-1">
+          <Counter start={3} />
+        </div>
       </div>
     </>
   );
